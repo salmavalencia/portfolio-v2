e5f3afba0bd4064b34adf031f76b5fd5bd0414e8
salmavalencia <salma@minnekdigital.com>
2024-02-13 09:19:18 -0400

    add container var tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1100px",
+      },
+    },
   },
   plugins: [],
 }
