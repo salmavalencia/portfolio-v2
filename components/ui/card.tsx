92495332e438c6dd9e0c7c487643d1741d3bf55e
salma <salmavalencia4@gmail.com>
2024-02-25 19:41:31 -0400

    add arbitrary props to card

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -3,15 +3,16 @@ import { twMerge } from "tailwind-merge";
 type CardProps = {
   children?: React.ReactNode;
   className?: string;
-};
+} & React.HTMLAttributes<HTMLDivElement>;
 
-export default function Card({ children, className }: CardProps) {
+export default function Card({ children, className, ...props }: CardProps) {
   return (
     <div
       className={twMerge(
         "w-full bg-navy-light rounded-md shadow-md p-5",
         className
       )}
+      {...props}
     >
       {children}
     </div>
