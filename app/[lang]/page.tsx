632b62b6f0b44343c406ed03893ceb4688175dd8
sal <salmavalencia4@gmail.com>
2025-01-29 20:14:50 +0100

    delete meta state, rearrange code and add loading animation

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -10,23 +10,12 @@ import Contact from "@/components/contact";
 import { useState, useCallback, useEffect } from "react";
 import { fetchAPI } from "./utils/fetch-api";
 
-interface Meta {
-  pagination: {
-    start: number;
-    limit: number;
-    total: number;
-  };
-}
-
 export default function Home() {
   const { isOpen } = useStore();
-  const [meta, setMeta] = useState<Meta | undefined>();
   const [data, setData] = useState<any>([]);
   const [isLoading, setLoading] = useState(true);
 
   const fetchData = useCallback(async (start: number, limit: number) => {
-    setLoading(true);
-
     try {
       const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
       const path = `/landing-pages`;
@@ -43,7 +32,6 @@ export default function Home() {
       } else {
         setData((prevData: any[] ) => [...prevData, ...responseData.data]);
       }
-      setMeta(responseData.meta);
     } catch (error) {
       console.error(error);
     } finally {
@@ -57,29 +45,50 @@ export default function Home() {
 
   return (
     <>
-      <Header headerData={data[5]}/>
-      <div
-        className={`-mt-20 ${isOpen && "transition-all duration-500 blur-sm"}`}
-      >
-        <div className="flex flex-col h-screen">
-          <main className="flex flex-grow container">
-            {data[0] && <Main mainData={data[0]}/>}
-          </main>
-        </div>
-        <div className="mb-48 flex flex-grow justify-center container">
-          {data[1] && <AboutMe aboutData={data[1]}/>}
-        </div>
-        <div className="mb-48 flex flex-grow justify-center container">
-          {data[2] && <Experience experienceData={data[2]}/>}
-        </div>
-        <div className="container">
-          {data[3] && <Skills skillData={data[3]}/>}
-        </div>
-        <div className="container">
-          {data[4] && <Contact contactData={data[4]}/>}
-        </div>
-      </div>
-      <footer></footer>
+      {
+        isLoading ? (
+          <div className="flex h-screen">
+            <span className="relative flex size-10 text-center justify-center m-auto">  
+              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green opacity-75"></span>  
+              <span className="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+            </span>
+          </div>
+        ) : (
+          <>
+          <Header headerData={data[5]}/>
+          <div
+            className={`-mt-20 ${isOpen && "transition-all duration-500 blur-sm"}`}
+          >
+            <div className="flex flex-col h-screen">
+              <main className="flex flex-grow container">
+                {data[0] && <Main mainData={data[0]}/>}
+              </main>
+            </div>
+            <div className="mb-48 flex flex-grow justify-center container">
+              {data[1] && <AboutMe aboutData={data[1]}/>}
+            </div>
+            <div className="mb-48 flex flex-grow justify-center container">
+              {data[2] && <Experience experienceData={data[2]}/>}
+            </div>
+            <div className="container">
+              {data[3] && <Skills skillData={data[3]}/>}
+            </div>
+            <div className="container">
+              {data[4] && <Contact contactData={data[4]}/>}
+            </div>
+          </div>
+          <footer></footer>
+          </>
+        )
+      }
     </>
   );
+}
+
+interface Meta {
+  pagination: {
+    start: number;
+    limit: number;
+    total: number;
+  };
 }
